0d05a79c0a6963bb0e0e877eb563469203ec14e7
Asdre <michalych2014@yandex.ru>
2023-05-19 12:07:29 +0300

    performance reports

--- perfomance_report/plot_performance_report.py
+++ perfomance_report/plot_performance_report.py
@@ -6,8 +6,6 @@ class param:
     PATH_TO_SAVE = './performance_report.jpg'
     REPORT_PATH = './report.csv'
 
-df = pd.read_csv(param.REPORT_PATH, index_col=0).set_index('loss')
-
 losses = [['sigmoid_pairwise_loss', 'sigmoid_pairwise_diff_hess'],
           ['sigmoid_pairwise_loss_auc','sigmoid_pairwise_diff_hess_auc'],
           ['sigmoid_pairwise_loss_auc_exact', 'sigmoid_pairwise_diff_hess_auc_exact']]
@@ -19,25 +17,28 @@ losses_full_names = {'sigmoid_pairwise_loss' : 'Sigmoid Pairwise Loss',
                    'sigmoid_pairwise_loss_auc_exact' : "Sigmoid Pairwise Loss exact AUC", 
                    'sigmoid_pairwise_diff_hess_auc_exact' : "Sigmoid Pairwise Grad&Hess exact AUC"}
 
-fig, ax = plt.subplots(3, 2, figsize=param.figsize)
-for i, _loss_name in enumerate(losses):
-    for j, loss_name in enumerate(_loss_name):
-        loss_full_name = losses_full_names[loss_name]
-        
-        df_loss = df.loc[loss_name]
-        df_loss_cpu = df_loss[df_loss.device == 'cpu']
-        df_loss_gpu = df_loss[df_loss.device == 'cuda']
+def plot_performance_report(df, PATH_TO_SAVE=param.PATH_TO_SAVE):
+    fig, ax = plt.subplots(3, 2, figsize=param.figsize)
+    for i, _loss_name in enumerate(losses):
+        for j, loss_name in enumerate(_loss_name):
+            loss_full_name = losses_full_names[loss_name]
+            
+            df_loss = df.loc[loss_name]
+            df_loss_cpu = df_loss[df_loss.device == 'cpu']
+            df_loss_gpu = df_loss[df_loss.device == 'cuda']
 
-        ax[i, j].set_title(loss_full_name)
-        ax[i, j].set_xlabel('N')
-        ax[i, j].set_ylabel('[ms]')
-        
-        ax[i, j].plot(df_loss_cpu.N, df_loss_cpu["mean[ms]"], 'bd')
-        ax[i, j].plot(df_loss_cpu.N, df_loss_cpu["mean[ms]"], 'b', label='i5 10600KF')
+            ax[i, j].set_title(loss_full_name)
+            ax[i, j].set_xlabel('N')
+            ax[i, j].set_ylabel('[s]')
+            
+            ax[i, j].plot(df_loss_cpu.N, df_loss_cpu["mean[ms]"], 'bd')
+            ax[i, j].plot(df_loss_cpu.N, df_loss_cpu["mean[ms]"], 'b', label='i5 10600KF')
 
-        ax[i, j].plot(df_loss_gpu.N, df_loss_gpu["mean[ms]"], 'gd')
-        ax[i, j].plot(df_loss_gpu.N, df_loss_gpu["mean[ms]"], 'g', label = 'RTX 3060')
-        ax[i, j].legend()
-plt.savefig(param.PATH_TO_SAVE)
+            ax[i, j].plot(df_loss_gpu.N, df_loss_gpu["mean[ms]"], 'gd')
+            ax[i, j].plot(df_loss_gpu.N, df_loss_gpu["mean[ms]"], 'g', label = 'RTX 3060')
+            ax[i, j].legend()
+    plt.savefig(PATH_TO_SAVE,  bbox_inches='tight')
         
-    
+if __name__ == '__main__':
+    df = pd.read_csv(param.REPORT_PATH, index_col=0).set_index('loss')
+    plot_performance_report(df)
